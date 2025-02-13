f9a818042be165ecad11d84eb79c0c0acacd00e3
Divyesh Limbad <divyeshlimbad9090@gmail.com>
2025-02-13 10:24:09 +0530

    old flutter project

--- lib/databse/my_database.dart
+++ lib/databse/my_database.dart
@@ -1,15 +1,15 @@
-import 'package:sqflite/sqflite.dart';
-
-class MyDatabase{
-  static const String TBL_USER = 'Tbl_User';
-  static const String USER_ID = 'Tbl_User';
-  static const String NAME = 'Name';
-  static const String EMAIL = 'Email';
-  static const String MOBILE_NUMBER = 'Mobile_Number';
-  static const String DOB = 'Dob';
-  static const String GENDER = 'Gender';
-  static const String CITY = 'City';
-  static const String IS_FAVOURITE = 'IsFavourite';
-
-
-}
+// import 'package:sqflite/sqflite.dart';
+//
+// class MyDatabase{
+//   static const String TBL_USER = 'Tbl_User';
+//   static const String USER_ID = 'Tbl_User';
+//   static const String NAME = 'Name';
+//   static const String EMAIL = 'Email';
+//   static const String MOBILE_NUMBER = 'Mobile_Number';
+//   static const String DOB = 'Dob';
+//   static const String GENDER = 'Gender';
+//   static const String CITY = 'City';
+//   static const String IS_FAVOURITE = 'IsFavourite';
+//
+//
+// }
